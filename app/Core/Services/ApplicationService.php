b3d7a9cfe6a6bc2bd7e77f2fe82ffac165628c53
Viet Vu <jooservices@gmail.com>
2021-08-20 13:25:51 +0700

    Application settings

---
+++ app/Core/Services/ApplicationService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Core\Services;
+
+use App\Core\Models\Application;
+use Illuminate\Support\Collection;
+
+class ApplicationService
+{
+    private Application|Collection $models;
+
+    public function __construct()
+    {
+        $this->refresh();
+    }
+
+    public function save(string $name, string $key, $value)
+    {
+        if (!$this->models->has($name)) {
+            $model = Application::firstOrCreate([
+                'name' => $name,
+            ], [
+                'settings' => [],
+            ]);
+        }
+
+        $model = $model ?? $this->models->get($name);
+
+        $settings = $model->settings;
+        $settings[$key] = $value;
+        $model->update(['settings' => $settings]);
+        $this->models->put($name, $model);
+
+        return $this;
+    }
+
+    public function remove(string $name, string $key)
+    {
+        $model = $this->models->get($name);
+        $settings = $model->settings;
+        unset($settings[$key]);
+        $model->update(['settings' => $settings]);
+
+        return $this;
+    }
+
+    public function get(string $name, string $key, $default = null)
+    {
+        $model = $this->models->get($name);
+        $settings = $model->settings ?? [];
+
+        return $settings[$key] ?? $default;
+    }
+
+    public static function getConfig(string $name, string $key, $default = null)
+    {
+        return app(ApplicationService::class)->get($name, $key, $default);
+    }
+
+    public function refresh()
+    {
+        $this->models = Application::all()->keyBy('name');
+    }
+}
