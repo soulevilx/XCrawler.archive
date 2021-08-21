a9e6bea65e67fbcb90ebe7b58dbeabe00fd8e33c
Viet Vu <jooservices@gmail.com>
2021-08-21 18:36:46 +0700

    Update missed files

--- app/Jav/Tests/JavTestCase.php
+++ app/Jav/Tests/JavTestCase.php
@@ -16,18 +16,9 @@ use Tests\TestCase;
  */
 class JavTestCase extends TestCase
 {
-    protected MockInterface|LegacyMockInterface $mocker;
-
     public function setUp(): void
     {
         parent::setUp();
         $this->fixtures = __DIR__.'/Fixtures';
-
-        app()->bind(ResponseInterface::class, DomResponse::class);
-        $this->mocker = \Mockery::mock(XCrawlerClient::class);
-        $this->mocker->shouldReceive('setService')->andReturnSelf();
-        $this->mocker->shouldReceive('init')->andReturnSelf();
-        $this->mocker->shouldReceive('setHeaders')->andReturnSelf();
-        $this->mocker->shouldReceive('setContentType')->andReturnSelf();
     }
 }
