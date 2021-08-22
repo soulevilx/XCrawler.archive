465570b3fc9cbfa840ab9647b72e72ef65022f29
Viet Vu <jooservices@gmail.com>
2021-08-22 11:57:33 +0700

    Update Github Action for staging

--- app/Jav/Tests/Feature/Console/OnejavTestLive.php
+++ app/Jav/Tests/Feature/Console/OnejavTestLive.php
@@ -2,6 +2,7 @@
 
 namespace App\Jav\Tests\Feature\Console;
 
+use App\Jav\Models\Onejav;
 use App\Jav\Tests\JavTestCase;
 
 class OnejavTestLive extends JavTestCase
@@ -9,10 +10,12 @@ class OnejavTestLive extends JavTestCase
     public function testOnejavDaily()
     {
         $this->artisan('jav:onejav daily');
+        $this->assertFalse(Onejav::all()->isEmpty());
     }
 
     public function testOnejavRelease()
     {
         $this->artisan('jav:onejav release');
+        $this->assertFalse(Onejav::all()->isEmpty());
     }
 }
