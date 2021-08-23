3aad17c46617f40a3f682398cc6f7b25c50e84d1
Viet Vu <jooservices@gmail.com>
2021-08-23 23:47:48 +0700

    Coverage

---
+++ app/Jav/Tests/Unit/Models/WordPressPostModelTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jav\Tests\Unit\Models;
+
+use App\Core\Models\WordPressPost;
+use App\Jav\Models\Movie;
+use App\Jav\Tests\JavTestCase;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class WordPressPostModelTest extends JavTestCase
+{
+    public function testModel()
+    {
+        $movie = Movie::factory()->create();
+        $wordPress = WordPressPost::factory()->create([
+            'model_id' => $movie->id,
+            'model_type' => Movie::class,
+        ]);
+
+        $this->assertInstanceOf(MorphTo::class, $wordPress->model());
+        $this->assertInstanceOf(Movie::class, $wordPress->model);
+    }
+}
