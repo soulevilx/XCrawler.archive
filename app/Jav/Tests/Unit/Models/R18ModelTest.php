430fab2f89360f62ef8f2ebaa745f3a022177039
Viet Vu <jooservices@gmail.com>
2021-09-02 21:04:26 +0700

    Update Tests

--- app/Jav/Tests/Unit/Models/R18ModelTest.php
+++ app/Jav/Tests/Unit/Models/R18ModelTest.php
@@ -7,7 +7,6 @@ use App\Jav\Models\R18;
 use App\Jav\Services\Movie\Observers\MovieObserver;
 use Tests\TestCase;
 
-
 class R18ModelTest extends TestCase
 {
     public function testModel()
@@ -19,9 +18,6 @@ class R18ModelTest extends TestCase
 
         $this->assertNull($r18->movie);
 
-        /**
-         * @covers MovieObserver
-         */
         $r18->update([
             'state_code' => State::STATE_COMPLETED,
         ]);
@@ -30,6 +26,6 @@ class R18ModelTest extends TestCase
         $this->assertEquals($r18->dvd_id, $r18->movie->dvd_id);
         $this->assertEquals($r18->genres, $r18->movie->genres->pluck('name')->toArray());
         $this->assertEquals($r18->performers, $r18->movie->performers->pluck('name')->toArray());
-        $this->assertFalse($r18->movie->is_downloadable);
+        $this->assertFalse($r18->movie->isDownloadable());
     }
 }
