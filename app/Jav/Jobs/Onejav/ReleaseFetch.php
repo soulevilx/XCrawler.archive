461da643191822acece86144b0aa1fe31a63e686
Viet Vu <jooservices@gmail.com>
2021-09-04 07:30:28 +0700

    XCrawler-25 Onejav release overlap

--- app/Jav/Jobs/Onejav/ReleaseFetch.php
+++ app/Jav/Jobs/Onejav/ReleaseFetch.php
@@ -2,11 +2,13 @@
 
 namespace App\Jav\Jobs\Onejav;
 
+use App\Core\Services\ApplicationService;
 use App\Jav\Services\OnejavService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 
 class ReleaseFetch implements ShouldQueue
 {
@@ -17,5 +19,15 @@ class ReleaseFetch implements ShouldQueue
     public function handle(OnejavService $service)
     {
         $service->release();
+    }
+
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array
+     */
+    public function middleware()
+    {
+        return [new WithoutOverlapping(ApplicationService::getConfig('onejav', 'current_page', 1))];
     }
 }
