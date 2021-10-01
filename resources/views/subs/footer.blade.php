be70287d194937870536f82155ecc459943d0375
Viet Vu <jooservices@gmail.com>
2021-10-01 12:39:00 +0700

    Sonar fix

--- resources/views/subs/footer.blade.php
+++ resources/views/subs/footer.blade.php
@@ -4,24 +4,19 @@
             <div class="row text-muted">
                 <div class="col-6 text-start">
                     <p class="mb-0">
-                        <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>AdminKit Demo</strong></a>
+                        <a class="text-muted" href="https://soulevil.com" target="_blank" rel="noopener"><strong>SoulEvil</strong></a>
                         &copy;
                     </p>
                 </div>
                 <div class="col-6 text-end">
                     <ul class="list-inline">
                         <li class="list-inline-item">
-                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
+                            <a class="text-muted" href="https://facebook.com/SoulEvilX/" target="_blank" rel="noopener"><i class="fab fa-facebook"></i></a>
                         </li>
                         <li class="list-inline-item">
-                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
+                            <a class="text-muted" href="https://twitter.com/SoulEvil" target="_blank" rel="noopener"><i class="fab fa-twitter"></i></a>
                         </li>
-                        <li class="list-inline-item">
-                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
-                        </li>
+
                     </ul>
                 </div>
             </div>
