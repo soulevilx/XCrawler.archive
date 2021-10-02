7b77a731db331427856e1608e069c145c2e18b5d
Viet Vu <jooservices@gmail.com>
2021-10-02 18:26:51 +0700

    Test

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,13 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    public function handle($request, \Closure $next)
+    {
+        if (env('APP_ENV') === 'testing') {
+            return $next($request);
+        }
+
+        return parent::handle($request, $next);
+    }
 }
