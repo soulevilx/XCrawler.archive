dcda5ee2d76181d7139858f89f3abaa91ade6d46
Viet Vu <jooservices@gmail.com>
2021-10-06 22:59:46 +0700

    Improvement

--- app/Jav/Events/MovieCreated.php
+++ app/Jav/Events/MovieCreated.php
@@ -4,6 +4,9 @@ namespace App\Jav\Events;
 
 use App\Jav\Models\Movie;
 
+/**
+ * This event trigger whenever movie create completed with Genres & Performers
+ */
 class MovieCreated
 {
     public function __construct(public Movie $movie)
