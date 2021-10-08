dea21e07ac52bfd769b9d96e671bb7d2b4870293
Viet Vu <jooservices@gmail.com>
2021-10-08 19:45:24 +0700

    Wordpress template update

--- app/Jav/Mail/WordPressPost.php
+++ app/Jav/Mail/WordPressPost.php
@@ -5,6 +5,7 @@ namespace App\Jav\Mail;
 use App\Jav\Models\Movie;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
 
 class WordPressPost extends Mailable
 {
@@ -24,18 +25,33 @@ class WordPressPost extends Mailable
      */
     public function build()
     {
+        $genres = implode(', ', array_map(
+            function ($genre) {
+                return '<a href="https://soulevil.com/tag/' . Str::slug($genre) .'">' . $genre. '</a>';
+            },
+            $this->movie->genres()->pluck('name')->toArray()
+        ));
+        $performers = implode(', ', array_map(
+            function ($performer) {
+                return '<a href="https://soulevil.com/tag/' . Str::slug($performer) .'">' . $performer . '</a>';
+            },
+            $this->movie->performers()->pluck('name')->toArray()
+        ));
+
         return $this
             ->from(config('mail.from.address'), config('mail.from.name'))
             ->to(config('mail.to.address'), config('mail.to.name'))
             ->view('emails.movie')
             ->with([
                 'movie' => $this->movie,
-                'genres' => implode(', ', $this->movie->genres()->pluck('name')->toArray()),
-                'performers' => implode(', ', $this->movie->performers()->pluck('name')->toArray()),
+                'genres' => $genres,
+                'performers' => $performers,
                 'channels' => implode(', ', $this->movie->channels ?? []),
                 'onejav' => $this->movie->onejav,
                 'r18' => $this->movie->r18,
-                'sample' => $this->movie->r18->sample['high'] ?? null
+                'sample' => $this->movie->r18->sample['high'] ?? null,
+                'publicize' => 'off',
+                'status' => 'draft'
             ]);
     }
 }
--- resources/views/emails/movie.blade.php
+++ resources/views/emails/movie.blade.php
@@ -1,21 +1,29 @@
 [title {{ $movie->dvd_id }}]
 [category Adult,JAV]
 [tags {{ $genres }}, {{ $performers }}]
-[publicize off]
+[publicize {{$publicize ?? 'off'}}]
 [excerpt]{{ $movie->description }}[/excerpt]
-[status draft]
-<p>
-    <img src="{{ $movie->cover }}" rel="nofollow" alt="{{$movie->dvd_id}}"/>
-</p>
+[status {{$status ?? 'draft'}}]
 
-<quote>{{ $movie->description }}</quote>
+<!-- wp:image {"id":{{$movie->id}},"sizeSlug":"large","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-large is-style-default">
+    <img src="{{$movie->cover}}" alt="" class="wp-image-{{$movie->id}}"/>
+    <figcaption>{{$movie->dvd_id}}</figcaption>
+</figure>
+<!-- /wp:image -->
 
+<blockquote class="wp-block-quote"><p>{{ $movie->description }}</p></blockquote>
+
+<!-- wp:list -->
 <ul>
-    <li><strong>Performers:</strong> {{ $performers }}</li>
-    <li><strong>Genres:</strong> {{ $genres }}</li>
+    <li><strong>Performers:</strong> {!! $performers !!}</li>
+    <li><strong>Genres:</strong> {!! $genres !!}</li>
     <li><strong>Content ID:</strong> {{ $movie->content_id }}</li>
     <li><strong>DVD ID:</strong> {{ $movie->dvd_id }}</li>
-    <li><strong>Downloadable:</strong> {{ $movie->isDownloadable() ? 'YES' : '' }}</li>
+    @if($onejav)
+        <li><strong>Downloadable:</strong> <a href="https://onejav.com{{ $onejav->url }}" rel="nofollow">Onejav</a></li>
+        <li><strong>Size:</strong> {{ $onejav->size }}</li>
+    @endif
     @if($movie->director)
         <li><strong>Director:</strong> {{ $movie->director }}</li>
     @endif
@@ -28,35 +36,24 @@
     @if($movie->channels)
         <li><strong>Channels:</strong> {{ $channels }}</li>
     @endif
-
 </ul>
+<!-- /wp:list -->
 
 [more]
 
-@if($onejav)
-    <ul>
-        <li><strong>Size:</strong> {{ $onejav->size }}</li>
-    </ul>
-    <p>
-        <a href="https://onejav.com{{ $onejav->url }}" rel="nofollow">Onejav</a>
-    </p>
-@endif
-
 @if($r18)
     <p>
-        @if($sample)
-            <video src="{{ $sample }}"></video>
+        @if($r18->sample())
+            <video src="{{ $r18->sample() }}"></video>
         @endif
     </p>
-
-    <p>
-        @if(!empty($movie->r18->gallery))
+    @if(!empty($movie->r18->gallery))
+        <figure class="wp-block-gallery columns-3 is-cropped">
             @foreach ($movie->r18->gallery as $image)
-                <img src="{{$image['large']}}" alt="{{$movie->title}}"/>
+                <p>
+                    <img src="{{$image['large']}}" alt="{{$movie->title}}"/>
+                </p>
             @endforeach
-        @endif
-    </p>
-    <p>
-        <a href="{{ $r18->url }}" rel="nofollow">R18</a>
-    </p>
+        </figure>
+    @endif
 @endif
