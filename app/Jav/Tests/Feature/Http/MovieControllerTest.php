2fe7eaad6e4d5648828266148a5026d0379a0772
Viet Vu <jooservices@gmail.com>
2021-10-08 15:37:31 +0700

    Improve template

--- app/Jav/Tests/Feature/Http/MovieControllerTest.php
+++ app/Jav/Tests/Feature/Http/MovieControllerTest.php
@@ -2,8 +2,9 @@
 
 namespace App\Jav\Tests\Feature\Http;
 
+use App\Core\Services\ApplicationService;
 use App\Jav\Mail\WordPressPost as WordPressPostEmail;
-use App\Jav\Models\Movie;
+use App\Jav\Models\Onejav;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
@@ -12,12 +13,14 @@ class MovieControllerTest extends TestCase
     public function testPostToWordPress()
     {
         Mail::fake();
-        $movie = Movie::factory()->create();
 
-        $this->post('/jav/movies/' . $movie->dvd_id . '/to-wordpress')->assertStatus(200);
+        ApplicationService::setConfig('jav', 'enable_post_to_wordpress', false);
+        $onejav = Onejav::factory()->create();
 
-        Mail::assertSent(WordPressPostEmail::class, function ($mail) use ($movie) {
-            return $mail->movie->is($movie);
+        $this->post('/jav/movies/' . $onejav->movie->dvd_id . '/to-wordpress')->assertStatus(200);
+
+        Mail::assertSent(WordPressPostEmail::class, function ($mail) use ($onejav) {
+            return $mail->movie->is($onejav->movie);
         });
     }
 }
