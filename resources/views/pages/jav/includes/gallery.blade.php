2181732a711753af3806b0985edd05ad562cdec7
Viet Vu <jooservices@gmail.com>
2021-10-09 20:54:07 +0700

    Hotfix gallery

--- resources/views/pages/jav/includes/gallery.blade.php
+++ resources/views/pages/jav/includes/gallery.blade.php
@@ -3,11 +3,19 @@
         <div class="row">
             @foreach ($movie->r18->gallery as $image)
                 <div class="col-lg-6">
-                    <img
-                        src="https://via.placeholder.com/150"
-                        alt="{{$movie->title}}"
-                        data-src="{{$image['large']}}"
-                        class="w-100 shadow-1-strong rounded mb-4 lazy"/>
+                    @if(in_array('large', $image))
+                        <img
+                            src="https://via.placeholder.com/150"
+                            alt="{{$movie->title}}"
+                            data-src="{{$image['large']}}"
+                            class="w-100 shadow-1-strong rounded mb-4 lazy"/>
+                    @else
+                        <img
+                            src="https://via.placeholder.com/150"
+                            alt="{{$movie->title}}"
+                            data-src="{{$image}}"
+                            class="w-100 shadow-1-strong rounded mb-4 lazy"/>
+                    @endif
                 </div>
             @endforeach
         </div>
