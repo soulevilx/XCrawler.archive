7ff16d16765f0c0660c33ce6fed0d34d219ab20a
Viet Vu <jooservices@gmail.com>
2021-10-29 09:55:51 +0700

    Try to ignore MaxAttemptsExceededException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Queue\MaxAttemptsExceededException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,6 +14,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
+        MaxAttemptsExceededException::class,
     ];
 
     /**
