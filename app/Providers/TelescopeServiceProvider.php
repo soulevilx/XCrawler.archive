1a1e1045087d1229c865742d431d884b5cdb6f56
Viet Vu <jooservices@gmail.com>
2021-11-02 16:20:16 +0700

    Telescope for testing

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,9 +21,9 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-//            if ($this->app->environment('local')) {
-//                return true;
-//            }
+            if ($this->app->environment('local')) {
+                return true;
+            }
 
             return $entry->isReportableException() ||
                    $entry->isFailedRequest() ||
