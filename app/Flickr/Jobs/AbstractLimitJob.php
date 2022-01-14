7507d1d5d2b3c78fb15f7fb6110514952e9535eb
Viet Vu <jooservices@gmail.com>
2022-01-14 14:20:32 +0700

    Flickr queues

---
+++ app/Flickr/Jobs/AbstractLimitJob.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Flickr\Jobs;
+
+use App\Core\Jobs\AbstractStandardJob;
+use Spatie\RateLimitedMiddleware\RateLimited;
+
+abstract class AbstractLimitJob extends AbstractStandardJob
+{
+    /**
+     * The maximum number of unhandled exceptions to allow before failing.
+     *
+     * @var int
+     */
+    protected int $maxExceptions = 3;
+
+    protected int $allow = 3000;
+    protected int $releaseAfterMinutes = 60;
+    protected int $everyMinutes = 60;
+
+    public const JOB_KEY = 'api:flickr';
+
+    public function retryUntil(): \DateTime
+    {
+        return now()->addDay();
+    }
+
+    public function middleware()
+    {
+        if ('testing' === config('app.env')) {
+            return [];
+        }
+
+        $rateLimitedMiddleware = (new RateLimited())
+            ->key(self::JOB_KEY)
+            ->allow($this->allow) // Allow  job
+            ->everyMinutes($this->everyMinutes) // In minutes
+            ->releaseAfterMinutes($this->releaseAfterMinutes); // Release back to pool
+
+        return [$rateLimitedMiddleware];
+    }
+}
--- app/Flickr/Jobs/FlickrContacts.php
+++ app/Flickr/Jobs/FlickrContacts.php
@@ -3,21 +3,11 @@
 namespace App\Flickr\Jobs;
 
 use App\Core\Models\State;
-use App\Flickr\Jobs\Traits\HasFlickrMiddleware;
 use App\Flickr\Models\FlickrContact;
 use App\Flickr\Services\FlickrService;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
 
-class FlickrContacts implements ShouldQueue
+class FlickrContacts extends AbstractLimitJob
 {
-    use Dispatchable;
-    use InteractsWithQueue;
-    use Queueable;
-    use HasFlickrMiddleware;
-
     public function handle(FlickrService $service)
     {
         $service->contacts()->getListAll()->each(function ($contact) {
