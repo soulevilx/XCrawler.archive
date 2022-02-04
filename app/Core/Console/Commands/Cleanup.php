26235c9d825fdafde477921d1bb6f4ce207d1937
Viet Vu <jooservices@gmail.com>
2022-02-04 09:10:35 +0700

    Cleanup FlickrProcesses

---
+++ app/Core/Console/Commands/Cleanup.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Core\Console\Commands;
+
+use App\Core\Services\CleanupService;
+
+class Cleanup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'core:cleanup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cleanup';
+
+    public function handle(CleanupService $service)
+    {
+        $service->cleanup();
+    }
+}
