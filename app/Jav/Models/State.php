079b4a0c547c01b81383ebf9a4a872d8bb8a9337
Viet Vu <jooservices@gmail.com>
2022-04-21 17:47:57 +0700

    Move Jav state out of Core

--- app/Jav/Models/State.php
+++ app/Jav/Models/State.php
@@ -2,35 +2,7 @@
 
 namespace App\Jav\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-class State extends Model
+class State extends \App\Core\Models\State
 {
-    public const STATE_INIT = 'CSIN';
-    public const STATE_PENDING = 'CSPE';
-    public const STATE_PROCESSING = 'CSPR';
-    public const STATE_COMPLETED = 'CSCE';
-    public const STATE_FAILED = 'CSFA';
-
-    public const STATES = [
-        self::STATE_INIT => 'init',
-        self::STATE_PENDING => 'pending',
-        self::STATE_PROCESSING => 'processing',
-        self::STATE_COMPLETED => 'completed',
-        self::STATE_FAILED => 'failed',
-    ];
-
-    protected $fillable = [
-        'reference_code',
-        'entity',
-        'state',
-    ];
-
-    protected $casts = [
-        'reference_code' => 'string',
-        'entity' => 'string',
-        'state' => 'string',
-    ];
-
-    protected $table = 'states';
+    protected $table = 'jav_states';
 }
