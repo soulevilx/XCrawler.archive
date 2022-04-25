d8900e18325f14e57a2058026ba8c8c54537cfd1
Viet Vu <jooservices@gmail.com>
2022-04-25 14:53:53 +0700

    Update Tests

--- app/Jav/Tests/Unit/Services/SCuteServiceTest.php
+++ app/Jav/Tests/Unit/Services/SCuteServiceTest.php
@@ -3,9 +3,11 @@
 namespace App\Jav\Tests\Unit\Services;
 
 use App\Core\Services\Facades\Application;
+use App\Jav\Crawlers\SCuteCrawler;
 use App\Jav\Services\SCuteService;
 use App\Jav\Tests\JavTestCase;
 use App\Jav\Tests\Traits\SCuteMocker;
+use Jooservices\XcrawlerClient\Response\DomResponse;
 
 class SCuteServiceTest extends JavTestCase
 {
@@ -29,6 +31,25 @@ class SCuteServiceTest extends JavTestCase
 
         $this->assertEquals(2, Application::getInt(SCuteService::SERVICE_NAME, 'current_page'));
         $this->assertDatabaseCount('scutes', 31);
+    }
+
+    public function testReleaseFailed()
+    {
+        $this->assertEquals(0, Application::getInt(SCuteService::SERVICE_NAME, 'current_page'));
+        $this->assertEquals(121, Application::getInt(SCuteService::SERVICE_NAME, 'total_pages'));
+
+        $mocker = $this->getClientMock();
+        $mocker
+            ->shouldReceive('get')
+            ->andReturn($this->getErrorMockedResponse(new DomResponse()));
+
+        app()->instance(SCuteCrawler::class, new SCuteCrawler($mocker));
+
+        $service = app(SCuteService::class);
+        $service->release();
+
+        $this->assertEquals(1, Application::getInt(SCuteService::SERVICE_NAME, 'current_page'));
+        $this->assertEquals(121, Application::getInt(SCuteService::SERVICE_NAME, 'total_pages'));
     }
 
     public function testReleaseAtEndOfPages()
