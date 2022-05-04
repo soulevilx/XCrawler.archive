06c37b346d3a65a602de043d9a3cde1c72cff8db
Viet Vu <jooservices@gmail.com>
2022-05-04 08:57:33 +0700

    Migrate Movies to MovieIndex

--- app/Jav/Listeners/MovieEventSubscriber.php
+++ app/Jav/Listeners/MovieEventSubscriber.php
@@ -4,8 +4,7 @@ namespace App\Jav\Listeners;
 
 use App\Jav\Events\MovieCreated;
 use App\Jav\Events\Onejav\OnejavDownloadCompleted;
-use App\Jav\Models\MovieIndex;
-use App\Jav\Notifications\MovieCreatedNotification;
+use App\Jav\Services\Movie\MovieService;
 use Illuminate\Events\Dispatcher;
 
 class MovieEventSubscriber
@@ -17,20 +16,13 @@ class MovieEventSubscriber
 
     public function indexMovie(MovieCreated $event)
     {
-        $movieData = $event->movie->toArray();
-        unset($movieData['id']);
-        $movieData['genres'] = $event->movie->genres()->pluck('name')->toArray();
-        $movieData['performers'] = $event->movie->performers()->pluck('name')->toArray();
-
-        MovieIndex::create($movieData);
-
-        $event->movie->notify(new MovieCreatedNotification($event->movie));
+        app(MovieService::class)->createIndex($event->movie);
     }
 
     /**
      * Register the listeners for the subscriber.
      *
-     * @param Dispatcher $events
+     * @param  Dispatcher  $events
      *
      * @return void
      */
@@ -43,14 +35,14 @@ class MovieEventSubscriber
             [
                 MovieCreated::class,
             ],
-            self::class . '@indexMovie'
+            self::class.'@indexMovie'
         );
 
         $events->listen(
             [
                 OnejavDownloadCompleted::class
             ],
-            self::class . '@onOnejavDownloadCompleted'
+            self::class.'@onOnejavDownloadCompleted'
         );
     }
 }
