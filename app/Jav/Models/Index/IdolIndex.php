16cfb6ed0e807801d06948b05443b204c62da184
Viet Vu <jooservices@gmail.com>
2022-05-06 06:39:45 +0700

    Create IdolIndex

---
+++ app/Jav/Models/Index/IdolIndex.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Jav\Models\Index;
+
+use App\Core\Models\BaseMongo;
+
+class IdolIndex extends BaseMongo
+{
+    protected $collection = 'idols';
+
+    protected $guarded = [];
+}
