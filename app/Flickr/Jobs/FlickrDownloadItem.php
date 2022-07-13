45f30cc698aedb245d8794281ccbb398fab061a1
Viet Vu <jooservices@gmail.com>
2022-07-13 14:33:57 +0700

    Flickr download and upload to server via SFTP

--- app/Flickr/Jobs/FlickrDownloadItem.php
+++ app/Flickr/Jobs/FlickrDownloadItem.php
@@ -3,9 +3,11 @@
 namespace App\Flickr\Jobs;
 
 use App\Core\Jobs\BaseJob;
-use App\Core\Jobs\DownloadItem;
 use App\Core\Models\State;
+use App\Core\Services\SftpService;
 use App\Flickr\Models\FlickrDownloadItem as FlickrDownloadItemModel;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
 
 class FlickrDownloadItem extends BaseJob
 {
@@ -13,14 +15,33 @@ class FlickrDownloadItem extends BaseJob
     {
     }
 
-    public function handle()
+    public function handle(SftpService $service)
     {
         $this->downloadItem->setState(State::STATE_PROCESSING);
         $photo = $this->downloadItem->photo;
         $download = $this->downloadItem->download;
 
-        $url = $photo->getLargestSizeUrl();
+        $downloadUrl = $photo->getLargestSizeUrl();
 
-        DownloadItem::dispatch('/Flickr/' . $download->path, $url,  $this->downloadItem);
+        $storage = Storage::drive('downloads');
+
+        $saveToDir = 'Flickr/'.$download->path;
+        $saveToFile = $storage->path($saveToDir).'/'.trim(basename($downloadUrl), '/');
+
+        if (!$storage->exists($saveToDir)) {
+            $storage->createDir($saveToDir);
+        }
+
+        $file = fopen($saveToFile, 'wb');
+
+        if (!app()->environment('testing')) {
+            $client = app(Client::class);
+            $client->request('GET', $downloadUrl, ['sink' => $file]);
+        }
+
+        if ($service->put($saveToDir, $saveToFile)) {
+            unlink($saveToFile);
+            $this->downloadItem->setState(State::STATE_COMPLETED);
+        }
     }
 }
